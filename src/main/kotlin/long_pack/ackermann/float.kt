b26bfc777f7fada750e6ddca38370767c9278f35
Evgeniy.Zhelenskiy <zhelenskiy2000@yandex.ru>
2023-04-26 22:45:48 +0200

    Make ackermann long_packs true long_packs: pack only on return but not store packed

--- src/main/kotlin/long_pack/ackermann/float.kt
+++ src/main/kotlin/long_pack/ackermann/float.kt
@@ -1,16 +1,16 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
 package long_pack.ackermann
 
 import ComplexNumberConsumer
 import ComplexNumberPrinter
 
-@JvmInline
-value class ComplexFloat private constructor(@PublishedApi internal val data: Long) {
-    inline val real: Float get() = Float.fromBits((data shr 32).toInt())
-    inline val imaginary: Float get() = Float.fromBits(data.toInt())
+private inline fun ComplexFloat.encodeToLong() = real.toRawBits().toLong().shl(32) or imaginary.toRawBits().toLong().and(0xFFFFFFFF)
+private inline fun Long.decodeToComplexFloat(): ComplexFloat = ComplexFloat(Float.fromBits(shr(32).toInt()), Float.fromBits(toInt()))
 
-    constructor(x: Float, y: Float) : this(
-        x.toRawBits().toLong().shl(32) or y.toRawBits().toLong().and(0xFFFFFFFF)
-    )
+@JvmInline
+value class ComplexFloat(val real: Float, val imaginary: Float) {
+    
     override fun toString(): String = when {
         imaginary == 0.0f -> "$real"
         real == 0.0f -> when (imaginary) {
@@ -24,30 +24,31 @@ value class ComplexFloat private constructor(@PublishedApi internal val data: Lo
             else -> "$real${if (imaginary < 0.0f) "" else "+"}${imaginary}i"
         }
     }
-    operator fun plus(other: ComplexFloat) = ComplexFloat(this.real + other.real, this.imaginary + other.imaginary)
-    operator fun plus(other: Float) = ComplexFloat(this.real + other, this.imaginary)
-    operator fun minus(other: ComplexFloat) = ComplexFloat(this.real - other.real, this.imaginary - other.imaginary)
-    operator fun minus(other: Float) = ComplexFloat(this.real - other, this.imaginary)
+    operator fun plus(other: ComplexFloat) = ComplexFloat(this.real + other.real, this.imaginary + other.imaginary).encodeToLong()
+    operator fun plus(other: Float) = ComplexFloat(this.real + other, this.imaginary).encodeToLong()
+    operator fun minus(other: ComplexFloat) = ComplexFloat(this.real - other.real, this.imaginary - other.imaginary).encodeToLong()
+    operator fun minus(other: Float) = ComplexFloat(this.real - other, this.imaginary).encodeToLong()
 }
 
-private val i: ComplexFloat get() = ComplexFloat(0.0f, 1.0f)
-private operator fun Float.times(complex: ComplexFloat) = ComplexFloat(this * complex.real, this * complex.imaginary)
-private operator fun Float.plus(complex: ComplexFloat) = ComplexFloat(this + complex.real, complex.imaginary)
+private val i: Long get() = ComplexFloat(0.0f, 1.0f).encodeToLong()
+private operator fun Float.times(complex: ComplexFloat) = ComplexFloat(this * complex.real, this * complex.imaginary).encodeToLong()
+private operator fun Float.plus(complex: ComplexFloat) = ComplexFloat(this + complex.real, complex.imaginary).encodeToLong()
 
-private val Float.complex get() = ComplexFloat(this, 0.0f)
-fun ackermann(a: ComplexFloat, b: ComplexFloat, n: Float): ComplexFloat = when {
-    n == 0.0f -> a + b
+private val Float.complex get() = ComplexFloat(this, 0.0f).encodeToLong()
+fun ackermann(a: ComplexFloat, b: ComplexFloat, n: Float): Long = when {
+    n == 0.0f -> (a + b)
     b.real == 0.0f -> when (val newN = n - 1.0f) {
         0.0f, 1.0f -> newN.complex
-        else -> a
+        else -> a.encodeToLong()
     }
-    else -> ackermann(a, ackermann(a, b - 1.0f, n), n - 1.0f)
+    else -> ackermann(a, ackermann(a, (b - 1.0f).decodeToComplexFloat(), n).decodeToComplexFloat(), n - 1.0f)
 }
 
 fun heavyActionFloat(complexConsumer: ComplexNumberConsumer) {
     for (n in 0..2) {
         for (x in 0..5) {
-            ackermann(x.toFloat() + 2.0f * i, x.toFloat().complex, n.toFloat())
+            ackermann((x.toFloat() + (2.0f * i.decodeToComplexFloat()).decodeToComplexFloat()).decodeToComplexFloat(), x.toFloat().complex.decodeToComplexFloat(), n.toFloat())
+                .decodeToComplexFloat()
                 .let { complexConsumer.consume(it.real, it.imaginary) }
         }
     }
--- src/main/kotlin/long_pack/ackermann/int.kt
+++ src/main/kotlin/long_pack/ackermann/int.kt
@@ -1,14 +1,16 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
 package long_pack.ackermann
 
 import ComplexNumberConsumer
 import ComplexNumberPrinter
 
-@JvmInline
-value class ComplexInt private constructor(@PublishedApi internal val data: Long) {
-    inline val real: Int get() = (data shr 32).toInt()
-    inline val imaginary: Int get() = data.toInt()
+private inline fun ComplexInt.encodeToLong() = real.toLong().shl(32) or imaginary.toLong().and(0xFFFFFFFF)
+private inline fun Long.decodeToComplexInt(): ComplexInt = ComplexInt(shr(32).toInt(), toInt())
 
-    constructor(x: Int, y: Int) : this(x.toLong().shl(32) or y.toLong().and(0xFFFFFFFF))
+@JvmInline
+value class ComplexInt(val real: Int, val imaginary: Int) {
+    
     override fun toString(): String = when {
         imaginary == 0 -> "$real"
         real == 0 -> when (imaginary) {
@@ -22,30 +24,32 @@ value class ComplexInt private constructor(@PublishedApi internal val data: Long
             else -> "$real${if (imaginary < 0) "" else "+"}${imaginary}i"
         }
     }
-    operator fun plus(other: ComplexInt) = ComplexInt(this.real + other.real, this.imaginary + other.imaginary)
-    operator fun plus(other: Int) = ComplexInt(this.real + other, this.imaginary)
-    operator fun minus(other: ComplexInt) = ComplexInt(this.real - other.real, this.imaginary - other.imaginary)
-    operator fun minus(other: Int) = ComplexInt(this.real - other, this.imaginary)
+    operator fun plus(other: ComplexInt) = ComplexInt(this.real + other.real, this.imaginary + other.imaginary).encodeToLong()
+    operator fun plus(other: Int) = ComplexInt(this.real + other, this.imaginary).encodeToLong()
+    operator fun minus(other: ComplexInt) = ComplexInt(this.real - other.real, this.imaginary - other.imaginary).encodeToLong()
+    operator fun minus(other: Int) = ComplexInt(this.real - other, this.imaginary).encodeToLong()
 }
 
-private val i: ComplexInt get() = ComplexInt(0, 1)
-private operator fun Int.times(complex: ComplexInt) = ComplexInt(this * complex.real, this * complex.imaginary)
-private operator fun Int.plus(complex: ComplexInt) = ComplexInt(this + complex.real, complex.imaginary)
+private val i: Long get() = ComplexInt(0, 1).encodeToLong()
+private operator fun Int.times(complex: ComplexInt) = ComplexInt(this * complex.real, this * complex.imaginary).encodeToLong()
+private operator fun Int.plus(complex: ComplexInt) = ComplexInt(this + complex.real, complex.imaginary).encodeToLong()
 
-private val Int.complex get() = ComplexInt(this, 0)
-fun ackermann(a: ComplexInt, b: ComplexInt, n: Int): ComplexInt = when {
-    n == 0 -> a + b
+private val Int.complex get() = ComplexInt(this, 0).encodeToLong()
+fun ackermann(a: ComplexInt, b: ComplexInt, n: Int): Long = when {
+    n == 0 -> (a + b)
     b.real == 0 -> when (val newN = n - 1) {
         0, 1 -> newN.complex
-        else -> a
+        else -> a.encodeToLong()
     }
-    else -> ackermann(a, ackermann(a, b - 1, n), n - 1)
+    else -> ackermann(a, ackermann(a, (b - 1).decodeToComplexInt(), n).decodeToComplexInt(), n - 1)
 }
 
 fun heavyActionInt(complexConsumer: ComplexNumberConsumer) {
     for (n in 0..2) {
         for (x in 0..5) {
-            ackermann(x + 2 * i, x.complex, n).let { complexConsumer.consume(it.real, it.imaginary) }
+            ackermann((x.toInt() + (2 * i.decodeToComplexInt()).decodeToComplexInt()).decodeToComplexInt(), x.toInt().complex.decodeToComplexInt(), n.toInt())
+                .decodeToComplexInt()
+                .let { complexConsumer.consume(it.real, it.imaginary) }
         }
     }
 }
